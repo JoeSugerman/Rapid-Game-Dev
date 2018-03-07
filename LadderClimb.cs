3128efc9836cd7c792881c271fc39563fb0826a4
JoeSugerman <36205071+JoeSugerman@users.noreply.github.com>
2018-03-07 13:53:39 -0500

    Add files via upload

---
+++ LadderClimb.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderClimb : MonoBehaviour {
+
+    public PlayerMove player;
+
+    // Use this for initialization
+    void Start()
+    {
+      
+
+    }
+
+    void OnTriggerEnter(Collider other) //placed on object you want to climb, will update player to be on ladder allowing vertical movmenet
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.onLadder = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.onLadder = false;
+        }
+    }
+}
