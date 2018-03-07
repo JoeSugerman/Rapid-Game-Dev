a82ab5d3eadb229072ab4145e7913d1e8609e69f
JoeSugerman <36205071+JoeSugerman@users.noreply.github.com>
2018-03-07 13:40:33 -0500

    Update NoiseSize.cs
    updated to store lifetime of object and function to determine if object should be destroyed or not.

--- NoiseSize.cs
+++ NoiseSize.cs
@@ -5,32 +5,19 @@ using UnityEngine;
 public class NoiseSize : MonoBehaviour {
 
     public float lifeTime;
-    public GameObject noise; 
-    
+       
 
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-   
-
-    public void IsNoiseOver()
+    public bool IsNoiseOver()
     {
-        if (lifeTime < 0)
+        if (lifeTime < 0) // check if life is over - if so return true so object will be destoryed
         {
-            Destroy(noise);
+            return true;
         }
         else
         {
-            noise.transform.localScale += new Vector3(1.0f, 1.0f, 1.0f);
-            lifeTime-=Time.deltaTime;
+            this.transform.localScale += new Vector3(1.0f, 1.0f, 1.0f); //if object is still alive, increase scale of nosie sphere
+            lifeTime-=Time.deltaTime;                                   //decrease life
+            return false;                                               //return false so object isn't destroyed
         }
     }
 }
